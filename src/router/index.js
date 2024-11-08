046507b1f77b813186b2cc42025cb139cb329a5e
victor muasya <vicmwe184@gmail.com>
2024-11-08 12:53:12 +0300

    correct file structure & code base reinitialization

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LoginView from "@/views/auth/LoginView.vue"
 import RegisterView from "@/views/auth/view/RegisterView.vue"
 import TheDashboardView from "@/components/TheDashboardView.vue";
-import TransactionView from "@/views/transactions/TransactionView.vue";
-import ViewSingleInvoice from "@/views/invoices/ViewSingleInvoice.vue";
-import InvoiceList from "@/views/invoices/InvoiceList.vue";
 import CustomerList from "@/views/customers/CustomerList.vue";
 import CreateEditCustomer from "@/views/customers/CreateEditCustomer.vue";
-import CreateInvoice from "@/views/invoices/CreateInvoice.vue";
-import OrganizationView from "@/views/organization/OrganizationView.vue";
-import BookingForm from "@/components/BookingForm.vue";
 import Landing from "@/components/Landing.vue";
 import about from "@/views/about.vue";
 import BookingList from "@/views/bookings/BookingList.vue";
