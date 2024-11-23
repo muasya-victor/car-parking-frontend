5c474a7c017a2c90207aef26f92a737907c4ea71
victor muasya <vicmwe184@gmail.com>
2024-11-23 07:36:06 +0300

    hide slots table from users

--- src/utility/constants.js
+++ src/utility/constants.js
@@ -1,5 +1,5 @@
-// export const baseUrl = "https://car-parking-backend-production.up.railway.app/api/"
-export const baseUrl = "http://localhost:8000/api/";
+export const baseUrl = "https://car-parking-backend-production.up.railway.app/api/"
+// export const baseUrl = "http://localhost:8000/api/";
 // export const baseUrl = "https://api.fastduka.co.ke/api/"
 export const headers = {
     "Content-Type": "application/json",
